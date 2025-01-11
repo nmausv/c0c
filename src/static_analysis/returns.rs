111fbe867cfbf89819ab892aaf7519173930e11e
Nick Maus <nmausv@gmail.com>
2025-01-11 11:10:30 -0500

    added return checks

---
+++ src/static_analysis/returns.rs
@@ -0,0 +1,25 @@
+use crate::frontend::elab_ast::{Program, Stmt};
+
+fn return_check_statement(s: &Stmt) -> bool {
+    match s {
+        Stmt::Declare(_, _, scope) => return_check_statement(scope),
+        Stmt::Assign(_, _) => false,
+        Stmt::Nop => false,
+        Stmt::Seq(v) => v.iter().any(return_check_statement),
+        Stmt::Exp(_) => false,
+        Stmt::While { .. } => false,
+        Stmt::If {
+            branch_true,
+            branch_false,
+            ..
+        } => {
+            return_check_statement(branch_true)
+                && return_check_statement(branch_false)
+        }
+        Stmt::Return(_) => true,
+    }
+}
+
+pub fn return_check(program: &Program) -> bool {
+    return_check_statement(&program.0)
+}
